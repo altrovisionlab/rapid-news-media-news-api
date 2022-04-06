697acca6f524bba9667ad9459bf3ff961c934f13
Sergiu, Dobrovolschi <altrovisionlab@gmail.com>
2022-04-06 23:49:13 +0100

    first commit

---
+++ Models/NewsReport.cs
@@ -0,0 +1,14 @@
+namespace rapid_news_media_news_api.Models
+{
+    public class NewsReport
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Category {get; set;}
+        public string? Description { get; set; }
+        public string? ImageUrl {get; set;}
+        public string? CreatedBy { get; set;}
+        public DateTime? DateCreated {get; set;}
+        public DateTime? LastModified {get; set;}
+    }
+}
